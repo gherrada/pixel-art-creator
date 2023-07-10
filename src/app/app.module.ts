99981ba88f806788812a8ac322952360f5cf62c1
gherrada <gonzalo.herrada@gmail.com>
2023-07-10 14:02:19 -0400

    added main features components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { FeaturesModule } from './features/features.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    FeaturesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CanvasComponent } from './canvas/canvas.component';
+import { ToolsComponent } from './tools/tools.component';
+import { ColorsComponent } from './colors/colors.component';
+import { ControlsComponent } from './controls/controls.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CanvasComponent,
+    ToolsComponent,
+    ColorsComponent,
+    ControlsComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    CanvasComponent,ToolsComponent,
   ]
 })
 export class FeaturesModule { }
